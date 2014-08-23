68b63931e035e3b68239dc3162dd27ec8da53cab
Masataro Asai <guicho2.71828@gmail.com>
2014-08-23 18:05:09 +0900

    bugfix: constant objects might appear in the component plan, even if they are already in the other components.

--- src/dereference.lisp
+++ src/dereference.lisp
@@ -40,6 +40,15 @@
            ;; it is called var, but actually it may return the ignored
            ;; objects
            (or (when-let ((pair (assoc o alist))) (cdr pair))
+               (when (typep o 'pddl-constant)
+                 (warn "~a: This is included neither in the obj-var alist ~
+                        or in the ignored-objects list, however, appeared =
+                        in the component plan. This is because
+                        it has a type ~a (a member of abstract type) ~
+                        but is a constant defined in the domain. ~
+                        Currently we don't treat it as an error."
+                       o (type o))
+                 o)
                (error "Parameter ~a not found" o))))
     (match f
       ((pddl-atomic-state name parameters)
@@ -75,13 +84,14 @@
                ;; if the default-alist is non-nil, it means that this
                ;; action is not a macro-action, but a partially grounded
                ;; action instantiated for decoding the macro-action later.
-               ;; The partially dereferenced action 
+               ;; The number of parameters should be = to that
+               ;; of the original action.
                (mapcar (lambda (p) (cdr (assoc p alist))) parameters)
                ;; If the default-alist is nil, this action is a
                ;; macro-action. It should remove the ignored-objects from
                ;; the alist. Otherwise, the parameter list in the domain
                ;; description contains a constant, resulting in an
-               ;; infeasible PDDL description. Also, it suppress the
+               ;; infeasible PDDL description. Also, it causes the
                ;; explosion during the translation.
                (iter (for (orig . var) in alist)
                      (unless (find orig ignored-objects)
